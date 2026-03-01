87d96131dde40bfd583c6dd912f0a71f2003bfe5
PNL <pgalvisg8156@universidadean.edu.co>
2026-03-01 13:35:42 -0500

    fix: storageBucket correcto (.firebasestorage.app)

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -8,8 +8,7 @@ const firebaseConfig = {
     apiKey: "AIzaSyCr3u2ZNrhBSSxnSx1-9VzPouRktipGRbk",
     authDomain: "physicsnoteslab-a23d9.firebaseapp.com",
     projectId: "physicsnoteslab-a23d9",
-    // Bucket correcto (formato estándar): <project>.appspot.com
-    storageBucket: "physicsnoteslab-a23d9.appspot.com",
+    storageBucket: "physicsnoteslab-a23d9.firebasestorage.app",
     messagingSenderId: "858001271509",
     appId: "1:858001271509:web:856f5bceb082bbd69f905c"
 };
